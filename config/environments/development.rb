74289b994a0e4eebc7ea3675cb12270f6535a23a
Ian Mitchell <ian.mitchel1@live.com>
2014-04-06 10:57:31 -0700

    Added Vote Sorting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,15 +39,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            'studyguardx@gmail.com',
-    password:             'letsdothis!',
-    authentication:       :plain,
-    enable_starttls_auto: true
-  }
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.smtp_settings = {
+  #  address:              'smtp.gmail.com',
+  #  port:                 587,
+  #  domain:               'example.com',
+  #  user_name:            'studyguardx@gmail.com',
+  #  password:             'letsdothis!',
+  #  authentication:       :plain,
+  #  enable_starttls_auto: true
+  #}
   config.action_mailer.raise_delivery_errors = true
 end
