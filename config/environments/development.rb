c6a0d5232bb2f773b6f9485660dcca3cfe62ad58
Talha  Saifi <tsaifi9@gmail.com>
2014-04-05 20:50:15 -0700

    Mailer now sends to email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,14 +38,14 @@ Rails.application.configure do
    #Stuff that Ian told me to do
    config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-   config.action_mailer.delivery_method = :letter_opener
-   #config.action_mailer.delivery_method = :smtp
-   #config.action_mailer.smtp_settings = {
-    # address:              'smtp.gmail.com',
-    # port:                 587,
-    # domain:               'example.com',
-    # user_name:            '<studyguardx>',
-    # password:             '<letsdothis!>',
-     #authentication:       'plain',
-     #enable_starttls_auto: true  }
+   #config.action_mailer.delivery_method = :letter_opener
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address:              'smtp.gmail.com',
+     port:                 587,
+     domain:               'example.com',
+     user_name:            'studyguardx@gmail.com',
+     password:             'letsdothis!',
+     authentication:       'plain',
+     enable_starttls_auto: true  }
 end
